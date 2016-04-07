9cfe3c3d80c2ed79d11b3bb88a678e20599f9e89
Haoran Feng <haoran.feng@sap.com>
2016-04-07 17:06:44 +0800

    finish echo server and client

---
+++ network/012_echo_client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host:port\n", os.Args[0])
+		os.Exit(1)
+	}
+	rAddr, err := net.ResolveTCPAddr("tcp4", os.Args[1])
+	checkError(err)
+
+	lAddr, err := net.ResolveTCPAddr("tcp4", "localhost:9876")
+	checkError(err)
+
+	conn, err := net.DialTCP("tcp", lAddr, rAddr)
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+		checkError(err)
+
+		_, err2 := conn.Write([]byte(line))
+		checkError(err2)
+
+		netReader := bufio.NewReader(conn)
+		result, err := netReader.ReadString('\n')
+		checkError(err)
+		fmt.Printf("%s", string(result))
+	}
+
+	os.Exit(0)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error : %s", err.Error())
+		os.Exit(1)
+	}
+}
