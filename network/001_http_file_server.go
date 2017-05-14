268ec5ebee4ba991c65cde3bfadf318c631e1847
Harri Feng <harrifeng@gmail.com>
2017-05-14 09:20:53 +0800

    we can point out the folder

--- network/001_http_file_server.go
+++ network/001_http_file_server.go
@@ -1,7 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func main() {
-	http.ListenAndServe(":8081", http.FileServer(http.Dir(".")))
+	shareFolder := "."
+	if len(os.Args) > 1 {
+		shareFolder = os.Args[1]
+	}
+	http.ListenAndServe(":8081", http.FileServer(http.Dir(shareFolder)))
 }
