d24de3f7c57cb6b0140b714516551713a1da7a8e
Haoran Feng <haoran.feng@alo7.com>
2017-09-18 14:09:12 +0800

    handler is an object handler function is a function HandlerFunction is a type that wrap one `hendler function` to a handler

---
+++ gwp/3-7/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type HelloHandler struct{}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+type WorldHandler struct{}
+
+func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World!")
+}
+
+func main() {
+
+	hello := HelloHandler{}
+	world := WorldHandler{}
+
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+
+	http.Handle("/hello", &hello)
+	http.Handle("/world", &world)
+
+	server.ListenAndServe()
+
+	fmt.Println()
+	os.Exit(0)
+}
