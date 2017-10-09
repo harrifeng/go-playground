7a6d04c0842fcd87a7bef722facf9828ec6f6244
Harri Feng <harrifeng@gmail.com>
2017-10-09 16:17:29 +0800

    new test

--- network/014_reverse_proxy_server.go
+++ network/014_reverse_proxy_server.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "net/http/httputil"
-    "net/url"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 type handle struct {
-    host string
-    port string
+	host string
+	port string
 }
 
 func (this *handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    remote, err := url.Parse("http://" + this.host + ":" + this.port)
-    if err != nil {
-        panic(err)
-    }
-    proxy := httputil.NewSingleHostReverseProxy(remote)
-    proxy.ServeHTTP(w, r)
+	remote, err := url.Parse("http://" + this.host + ":" + this.port)
+	if err != nil {
+		panic(err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.ServeHTTP(w, r)
 }
 
 func startServer() {
-    //被代理的服务器host和port
-    h := &handle{host: "127.0.0.1", port: "9001"}
-    err := http.ListenAndServe(":80", h)
-    if err != nil {
-        log.Fatalln("ListenAndServe: ", err)
-    }
+	//被代理的服务器host和port
+	h := &handle{host: "127.0.0.1", port: "9001"}
+	err := http.ListenAndServe(":80", h)
+	if err != nil {
+		log.Fatalln("ListenAndServe: ", err)
+	}
 }
 
 func main() {
-    startServer()
+	startServer()
 }
---
+++ seelogtest/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	seelog "github.com/cihub/seelog"
+)
+
+func main() {
+	logger, err := seelog.LoggerFromConfigAsFile("seelog.xml")
+
+	defer logger.Flush()
+
+	if err != nil {
+		seelog.Critical("err parsing config log file", err)
+		return
+	}
+	seelog.ReplaceLogger(logger)
+
+	seelog.Error("seelog error")
+	seelog.Info("seelog info")
+	seelog.Debug("seelog debug")
+}
