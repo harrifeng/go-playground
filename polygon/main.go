377c4b5fd683b231bb6a6a8ae1841b7102a5b010
Haoran Feng <harrifeng@gmail.com>
2018-06-07 03:32:18 +0000

    go on

--- polygon/main.go
+++ polygon/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
-	"github.com/kellydunn/golang-geo"
+	geo "github.com/kellydunn/golang-geo"
 )
 
 func main() {
 
-	p := geo.Point{}
-	fmt.Println(p)
+	point := geo.Point{}
+	demo, err := polygonFromFile("./data/demo.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(demo.Contains(&point))
 	os.Exit(0)
+}
+
+type testPoints struct {
+	Points []*geo.Point
+}
+
+func polygonFromFile(filename string) (*geo.Polygon, error) {
+	p := &geo.Polygon{}
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	points := new(testPoints)
+	jsonParser := json.NewDecoder(file)
+	if err = jsonParser.Decode(&points); err != nil {
+		return nil, err
+	}
+
+	for _, point := range points.Points {
+		p.Add(point)
+	}
+
+	return p, nil
 }
